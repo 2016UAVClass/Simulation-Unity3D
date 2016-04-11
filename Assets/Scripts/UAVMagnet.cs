5cb48b07ad9b64c7bfae8c6dd82960d58ca2040b
Jonathan Schenker <jonathan.f.schenker@vanderbilt.edu>
2016-04-10 21:02:08 -0500

    Added Trap/Wind Simulation
    • Added Trap model
    • Added Trap connection scripts
    • Added wind force affecting UAV

---
+++ Assets/Scripts/UAVMagnet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UAVMagnet : MonoBehaviour 
+{
+
+	GameObject ConnectedTrap;
+
+	public void Release()
+	{
+		if(ConnectedTrap != null)
+		{
+			Destroy(ConnectedTrap.GetComponent<FixedJoint>());
+			ConnectedTrap = null;
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.GetComponent<TrapMagnet>() != null && ConnectedTrap == null)
+		{
+			GameObject Trap = col.GetComponentInParent<Rigidbody>().gameObject;
+			Trap.AddComponent<FixedJoint>();
+			Trap.GetComponent<FixedJoint>().connectedBody = GetComponent<Rigidbody>();
+			ConnectedTrap = Trap;
+		}
+	}
+}
---
+++ Assets/Scripts/Wind.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wind : MonoBehaviour {
+
+	private WindAffector[] UAVs;
+
+	[Range (0, 25)]
+	public float WindForce;
+	public Vector3 WindDirection;
+
+	private float WindForceConst = 4.5f;
+
+	void Start()
+	{
+		//Find all objects with WindAffector component at start of simulation
+		UAVs = FindObjectsOfType<WindAffector>();
+	}
+
+	void FixedUpdate ()
+	{
+		for(int i=0; i < UAVs.Length; i++)
+		{
+			Rigidbody UAVBody = UAVs[i].GetComponent<Rigidbody>();
+			if(UAVBody)
+			{
+				Vector3 wind = WindForce*WindForceConst*(WindDirection.normalized)*Random.Range(0.9f, 1.1f);
+				Debug.DrawRay(UAVBody.transform.position, wind/(WindForceConst*2), Color.blue);
+				UAVBody.AddForce(wind*Time.fixedDeltaTime);
+			}
+		}
+	}
+}
